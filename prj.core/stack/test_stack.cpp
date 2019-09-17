ec4a8ac0dc0ea263424d92430acad1ed330d82e5
iljuhin sergej <shuctrikc555@gmail.com>
2019-09-17 22:20:51 +0300

    add unit tests

--- prj.core/stack/test_stack.cpp
+++ prj.core/stack/test_stack.cpp
@@ -1,89 +1,81 @@
-#include "src/stack.h"
+#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include <string>
 #include <vector>
+#include <catch2/catch.hpp>
+#include "src/stack.h"
 
-bool TestCreate() {
-    try {
+TEST_CASE("Test Creat", "Stack") {
         Stack<int> a;
-        a.Push(1);
+        REQUIRE_NOTHROW(a.Push(1));
         Stack<double> b;
-        b.Push(1.0);
+        REQUIRE_NOTHROW(b.Push(1.0));
         Stack<char> c;
-        c.Push('1');
+        REQUIRE_NOTHROW(c.Push('1'));
         Stack<std::string> d;
-        d.Push("1");
+        REQUIRE_NOTHROW(d.Push("1"));
         Stack<std::vector<int>> e;
-        e.Push({1});
-    } catch (const std::exception &ex) {
-        ::fprintf(stderr, "%s\n", ex.what());
-        ::fprintf(stdout, "TestCreate - Fail\n");
-        return false;
+        REQUIRE_NOTHROW(e.Push({1}));
+}
+
+
+TEST_CASE("Test Push Pop", "Stack") {
+    Stack<int> st;
+    for (size_t i(0); i < 100; ++i) {
+        REQUIRE_NOTHROW(st.Push(random() % 100));
+        REQUIRE_NOTHROW(st.Pop());
     }
-    ::fprintf(stdout, "TestCreate - Complete\n");
-    return true;
+    Stack<double> b;
+    for (size_t i(0); i < 100; ++i)
+        REQUIRE_NOTHROW(b.Push(1.0));
+    for (size_t i(0); i < 100; ++i)
+        REQUIRE_NOTHROW(b.Pop());
 }
 
-bool TestPushPop() {
-    try {
-        Stack<int> a;
-        for (size_t i(0); i < 100; ++i) {
-            a.Push(i);
-            a.Pop();
-        }
+TEST_CASE("Test Top Empty Stack", "Stack") {
+    Stack<int> st;
+    REQUIRE_NOTHROW(st.Push(1));
+    REQUIRE_NOTHROW(st.Pop());
+    REQUIRE_THROWS(st.Top());
+}
 
-        Stack<double> b;
-        for (size_t i(0); i < 100; ++i)
-            b.Push(1.0);
-        for (size_t i(0); i < 100; ++i)
-            b.Pop();
-
-    } catch (const std::exception &ex) {
-        ::fprintf(stderr, "%s\n", ex.what());
-        ::fprintf(stdout, "TestPushPop - Fail\n");
-        return false;
+TEST_CASE("Test Container", "Stack") {
+    Stack<int> st;
+    std::vector<int> st_copy;
+    st_copy.reserve(100);
+    for (int i(0); i < 100; ++i) {
+        st_copy.push_back(random() % 100);
+        st.Push(st_copy.back());
     }
-    ::fprintf(stdout, "TestPushPop - Complete\n");
-    return true;
-}
-
-bool TestCopy() {
-    try {
-        Stack<int> a;
-        for (size_t i(0); i < 100; ++i) {
-            a.Push(1);
-        }
-        Stack<int> b(a);
-        Stack<int> c;
-        for (size_t i(0); i < 10; ++i)
-            c.Push(2);
-        c = a;
-        a.Pop();
-        a.Push(2);
-        for (size_t i(0); i < 100; ++i) {
-            const int &c_top = c.Top();
-            const int &b_top = b.Top();
-            if (c_top != 1 || b_top != 1)
-                throw std::runtime_error("Error in Stack!\n");
-            c.Pop();
-            b.Pop();
-        }
-    } catch (const std::exception &ex) {
-        ::fprintf(stderr, "%s\n", ex.what());
-        ::fprintf(stdout, "TestCopy - Fail\n");
-        return false;
+    for (int i(99); i >= 0; --i) {
+        REQUIRE(st_copy[i] == st.Top());
+        st.Pop();
     }
-    ::fprintf(stdout, "TestCopy - Complete\n");
-    return true;
 }
 
+TEST_CASE("Test Copy", "Stack") {
+    Stack<int> st;
+    for (size_t i(0); i < 100; ++i)
+        st.Push(random() % 100);
 
-int main(int arc, char** argv) {
-    if (!TestCreate())
-        return 1;
-    if (!TestPushPop())
-        return 1;
-    if (!TestCopy())
-        return 1;
-    return 0;
+    Stack<int> st_copy_1(st);
+    Stack<int> st_copy_2;
+
+    for (size_t i(0); i < 10; ++i)
+        st_copy_2.Push(2);
+
+    st_copy_2 = st;
+    int st_top = st.Top();
+    for (size_t i(0); i < 100; ++i) {
+        int st_copy_1_top = st_copy_1.Top();
+        st_copy_1.Pop();
+        REQUIRE(st_copy_1_top == st_copy_2.Top());
+        st_copy_2.Pop();
+        REQUIRE(st_top == st.Top());
+    }
 }
+
+//int main(int arc, char** argv) {
+//    Catch::RunTests();
+//    return 0;
+//}
